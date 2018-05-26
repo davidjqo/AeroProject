367f2368a3b2243ef746cb56123a9bf6814110fa
ferdonavarro <ferdonavarro@gmail.com>
2018-05-25 18:51:17 -0600

    Update Horarios.java

--- app/src/main/java/com/example/fernando/aeroproject/Horarios.java
+++ app/src/main/java/com/example/fernando/aeroproject/Horarios.java
@@ -3,11 +3,20 @@ package com.example.fernando.aeroproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Horarios extends AppCompatActivity {
 
+public class Horarios extends AppCompatActivity {
+EditText dia, hora, ruta ,precio;
+Button agregarhorario;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_horarios);
+         
+        dia = (EditText)findViewById(R.id.horario1);
+        hora = (EditText)findViewById(R.id.horario2);
+        ruta = (EditText)findViewById(R.id.ruta);
+        precio = (EditText)findViewById(R.id.precio);
+
     }
 }
